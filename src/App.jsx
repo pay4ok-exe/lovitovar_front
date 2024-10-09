24daceaf73a7f6b802ca0a2f9efd94adb5628945
Akim Ospan <ospanakim05@gmail.com>
2024-10-10 02:47:29 +0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Signin from "./pages/Signin";
-import Signout from "./pages/Signout";
-import About from "./pages/About";
-import Profile from "./pages/Profile";
-import Header from "./components/Header";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/sign-in" element={<Signin />}></Route>
-        <Route path="/sign-out" element={<Signout />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/profile" element={<Profile />}></Route>
+        
       </Routes>
     </BrowserRouter>
   );
--- src/pages/SignOut.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Signout = () => {
-  return <div>Sign Out</div>;
-};
-
-export default Signout;
