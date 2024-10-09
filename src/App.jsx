5bf18a5aa3ddcb807a4b741a10895409620747de
Akim Ospan <ospanakim05@gmail.com>
2024-10-09 19:38:59 +0500

    Added Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signout from "./pages/Signout";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="text-emerald-300">
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/sign-in" element={<Signin />}></Route>
+        <Route path="/sign-out" element={<Signout />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
